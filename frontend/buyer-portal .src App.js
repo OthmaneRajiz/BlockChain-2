21d133aac191c4423e2aaf5d90f2740ff2abb7b8
Othmane Rajiz <othmanera0123@gmail.com>
2025-06-10 12:21:24 +0100

    Create buyer-portal .src App.js

---
+++ frontend/buyer-portal .src App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Buyer Portal</h1>
+      {/* Buyer portal UI components */}
+    </div>
+  );
+}
+
+export default App;
