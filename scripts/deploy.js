bae48cd7f0ea7fabbacdfe129a6166a9024db7ab
Othmane Rajiz <othmanera0123@gmail.com>
2025-06-10 12:32:19 +0100

    Smart contracts deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,11 @@
+// Script for deploying smart contracts
+async function main() {
+  // Deploy contracts using ethers.js
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
