347d792c5cd62e561848017886cd259bbc59b6c9
Othmane Rajiz <othmanera0123@gmail.com>
2025-06-10 12:21:59 +0100

    Create consumer-app .src App.js

---
+++ frontend/consumer-app .src App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Consumer App</h1>
+      {/* Consumer application UI components */}
+    </div>
+  );
+}
+
+export default App;
