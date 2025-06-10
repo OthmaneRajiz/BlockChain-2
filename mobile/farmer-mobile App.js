8d52edc46ce06634778f027aafb2945f9a00a24c
Othmane Rajiz <othmanera0123@gmail.com>
2025-06-10 12:31:30 +0100

    Create farmer-mobile App.js

---
+++ mobile/farmer-mobile App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Farmer Mobile App</Text>
+      {/* Farmer mobile app UI components */}
+    </View>
+  );
+}
+
+export default App;
